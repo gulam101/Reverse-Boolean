905b2de370f8261b082896d992ea322a6a047426
unknown <gfram@hotmail.com>
2019-09-22 11:48:56 +0100

    first commit

---
+++ Reverse-Boolean-master/Reverse Boolean/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Boolean
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            MainFunction();
+        }
+
+        public static void MainFunction()
+        {    
+                Console.WriteLine("Welcome to reverse boolean");
+                Console.Write("Please type in either true or false: ");
+                bool userinput;
+
+                try
+                {
+                    userinput = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine($"You have choosen: {userinput}");
+
+                    if (userinput)
+                    {
+                        Console.WriteLine("This is false!");
+                    
+                     if (userinput)
+                    {
+                        Console.ReadLine();
+                    }
+                }
+                    else if (!(userinput))
+                    {
+                        Console.WriteLine("This is true!");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("You haven't chosen a valid option");
+                    MainFunction();
+                }
+                Console.ReadLine();          
+        }
+    }
+}
